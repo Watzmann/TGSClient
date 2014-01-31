d7d28d927e2ebc9e2db16553710ea556432b798d
Andreas Hausmann <ajh.hausmann@gmail.com>
2014-01-31 12:26:50 +0100

    Metrics: restructured board_css.py.

--- administration/board_css.py
+++ administration/board_css.py
@@ -8,6 +8,13 @@ from StringIO import StringIO
 class BoardCss():
     def __init__(self,):
         """Create the boards CSS."""
+        self.board_css = self._points()
+        self.board_css += self._checkers()
+        self.board_css += self._dice()
+        self.board_css += self._decoration()
+
+    def _points(self,):
+        """Create the boards points."""
         css = StringIO()
         rule = "#p%(point)d { position:absolute; top:%(top)dpx; left:%(left)dpx; width:%(width)dpx; height:%(height)dpx; }"
         WIDTH = 31
@@ -31,20 +38,12 @@ class BoardCss():
                 width += BAR_WIDTH
             print >>css, rule % values
         print >>css, "#p0 { position:absolute; top:238px; left:468px; width:27px; height:120px; }"
-        print >>css, "#oDitch { position:absolute; top:31px; left:468px; width:27px; height:120px; }"
-        print >>css, "#oBar { position:absolute; top:220px; left:245px; width:27px; height:135px; }"
         print >>css, "#p25 { position:absolute; top:42px; left:245px; width:27px; height:135px; }"
-        print >>css, "#pDice1 { position:absolute; top:182px; left:338px; width:21px; height:21px; }"
-        print >>css, "#pDice2 { position:absolute; top:182px; left:398px; width:21px; height:21px; }"
-        print >>css, "#oDice1 { position:absolute; top:182px; left:129px; width:21px; height:21px; }"
-        print >>css, "#oDice2 { position:absolute; top:182px; left:189px; width:21px; height:21px; }"
-        print >>css, "#rollDice { position:absolute; top:178px; left:417px; width:38px; height:45px; }"
-        print >>css, "#sendMove { position:absolute; top:176px; left:335px; width:89px; height:47px; }"
-        print >>css, "#undo { position:absolute; top:188px; left:247px; width:22px; height:22px; }"
-        print >>css, ".upperpipsX { position:relative; top:131px; left:43px; }"
-        print >>css, ".lowerpipsX { position:relative; top:198px; left:43px; }"
-        print >>css, ".upperpipsO { position:relative; top:143px; left:43px; }"
-        print >>css, ".lowerpipsO { position:relative; top:186px; left:43px; }"
+        return css.getvalue()
+
+    def _checkers(self,):
+        """Create the boards checker positions."""
+        css = StringIO()
         print >>css, ".c5 { position:relative; top:-%dpx; }" % (4*27 + 13)
         print >>css, ".c9 { position:relative; top:-%dpx; }" % (8*27)
         offset = {5:8*27+13, 9:16*27+27, 12:22*27+40}
@@ -53,7 +52,30 @@ class BoardCss():
                 o = offset[i]
             print >>css, ".cp%d { position:relative; top:-%dpx; }" % \
                                             (i, 2*i*27 - o)
-        self.board_css = css.getvalue()
+        return css.getvalue()
+
+    def _dice(self,):
+        """Create the boards checker positions."""
+        css = StringIO()
+        print >>css, "#pDice1 { position:absolute; top:182px; left:338px; width:21px; height:21px; }"
+        print >>css, "#pDice2 { position:absolute; top:182px; left:398px; width:21px; height:21px; }"
+        print >>css, "#oDice1 { position:absolute; top:182px; left:129px; width:21px; height:21px; }"
+        print >>css, "#oDice2 { position:absolute; top:182px; left:189px; width:21px; height:21px; }"
+        print >>css, "#rollDice { position:absolute; top:178px; left:417px; width:38px; height:45px; }"
+        print >>css, "#sendMove { position:absolute; top:176px; left:335px; width:89px; height:47px; }"
+        return css.getvalue()
+
+    def _decoration(self,):
+        """Create the boards checker positions."""
+        css = StringIO()
+        print >>css, "#oDitch { position:absolute; top:31px; left:468px; width:27px; height:120px; }"
+        print >>css, "#oBar { position:absolute; top:220px; left:245px; width:27px; height:135px; }"
+        print >>css, "#undo { position:absolute; top:188px; left:247px; width:22px; height:22px; }"
+        print >>css, ".upperpipsX { position:relative; top:131px; left:43px; }"
+        print >>css, ".lowerpipsX { position:relative; top:198px; left:43px; }"
+        print >>css, ".upperpipsO { position:relative; top:143px; left:43px; }"
+        print >>css, ".lowerpipsO { position:relative; top:186px; left:43px; }"
+        return css.getvalue()
 
 if __name__ == '__main__':
     print "/* %-55s */" % "This css file is created automatically by board_css.py."
